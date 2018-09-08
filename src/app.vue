3767f8a1c47f6308403acc30d055687446989daf
charles <952046097@qq.com>
2018-09-08 14:02:15 +0800

    add

--- src/app.vue
+++ src/app.vue
@@ -1,91 +1,28 @@
-<script>
-import appConfig from '@src/app.config'
-
-export default {
-    page: {
-        // All subcomponent titles will be injected into this template.
-        titleTemplate(title) {
-            title = typeof title === 'function' ? title(this.$store) : title
-            return title ? `${title} | ${appConfig.title}` : appConfig.title
-        },
-    },
-}
-</script>
-
 <template>
   <div id="app">
-    <!--
-    Even when routes use the same component, treat them
-    as distinct and create the component again.
-    -->
-    <router-view :key="$route.fullPath" />
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="1Welcome to Your Vue.js App"/>
   </div>
 </template>
 
-<!-- This should generally be the only global CSS in the app. -->
-<style lang="scss">
-// Allow element/type selectors, because this is global CSS.
-// stylelint-disable selector-max-type, selector-class-pattern
+<script>
+import HelloWorld from './components/HelloWorld.vue'
 
-// Normalize default styles across browsers,
-// https://necolas.github.io/normalize.css/
-@import '~normalize.css/normalize';
-// Style loading bar between pages.
-// https://github.com/rstacruz/nprogress
-@import '~nprogress/nprogress';
-
-// Design variables and utilities from src/design.
-@import '@design';
-
-*,
-*::before,
-*::after {
-    box-sizing: border-box;
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
 }
+</script>
 
-body {
-    background: $color-body-bg;
-}
-
+<style>
 #app {
-    @extend %typography-small;
-}
-
-// ===
-// Base element styles
-// ===
-
-a,
-a:visited {
-    color: $color-link-text;
-}
-
-h1 {
-    @extend %typography-xxlarge;
-}
-
-h2 {
-    @extend %typography-xlarge;
-}
-
-h3 {
-    @extend %typography-large;
-}
-
-h4 {
-    @extend %typography-medium;
-}
-
-h5,
-h6 {
-    @extend %typography-small;
-}
-
-// ===
-// Vendor
-// ===
-
-#nprogress .bar {
-    background: $color-link-text;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
--- src/router/views/timeout.unit.js
+++
@@ -1,7 +0,0 @@
-import Timeout from './timeout'
-
-describe('@views/timeout', () => {
-  it('is a valid view', () => {
-    expect(Timeout).toBeAViewComponent()
-  })
-})
